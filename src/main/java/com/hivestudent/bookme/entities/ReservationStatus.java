4aa7039c248db3746b75fd5db58a12891984126c
Abdulbaki <Salausmart132@gmail.com>
2025-06-19 02:51:29 +0300

    Roles & status enum

---
+++ src/main/java/com/hivestudent/bookme/entities/ReservationStatus.java
@@ -0,0 +1,5 @@
+package com.hivestudent.bookme.entities;
+
+public enum ReservationStatus {
+    RESERVED, MY_RESERVATION, RESTRICTED, PAST
+}
---
+++ src/main/java/com/hivestudent/bookme/entities/Role.java
@@ -0,0 +1,6 @@
+package com.hivestudent.bookme.entities;
+
+public enum Role {
+    STUDENT,
+    STAFF
+}
