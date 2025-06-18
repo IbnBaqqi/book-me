4fdf509eb8f1bd2e7e9ea61f58e131f60bfebe82
Abdulbaki <Salausmart132@gmail.com>
2025-06-18 19:49:09 +0300

    initial commit

---
+++ src/main/java/com/hivestudent/bookme/BookMeApplication.java
@@ -0,0 +1,13 @@
+package com.hivestudent.bookme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMeApplication.class, args);
+    }
+
+}
