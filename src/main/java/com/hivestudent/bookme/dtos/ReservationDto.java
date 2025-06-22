1047d4249337d157c9814359b43b35e70bacbe62
Abdulbaki <Salausmart132@gmail.com>
2025-06-22 16:48:01 +0300

    minor update

--- src/main/java/com/hivestudent/bookme/dtos/ReservationDto.java
+++ src/main/java/com/hivestudent/bookme/dtos/ReservationDto.java
@@ -6,7 +6,6 @@ import java.time.LocalDateTime;
 
 @Data
 public class ReservationDto {
-    // don't send createdBy if role not staff or user isn't createdBy current user
     private Long id;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
