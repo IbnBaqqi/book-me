2cbf0455d1202ca8f08ee4e96850104c378b5093
Jordane Gengo <jordane.gengo@wolt.com>
2025-07-04 17:05:19 +0300

    feat: add jgengo.dev and hive.fi as allowed origins

--- src/main/java/com/hivestudent/bookme/Config/WebConfig.java
+++ src/main/java/com/hivestudent/bookme/Config/WebConfig.java
@@ -13,8 +13,12 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedOrigins("https://booking-calendar-chi.vercel.app")
+                        .allowedOriginPatterns(
+                            "http://localhost:5173",
+                            "https://booking-calendar-chi.vercel.app",
+                            "https://*.hive.fi",
+                            "https://*.jgengo.dev"
+                        )
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
