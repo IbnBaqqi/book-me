1cf488df143120631005542e1730c34e8b143591
Abdulbaki <Salausmart132@gmail.com>
2025-07-12 23:39:00 +0300

    test initial

---
+++ src/test/java/com/hivestudent/bookme/Auth/OAuthServiceTest.java
@@ -0,0 +1,19 @@
+package com.hivestudent.bookme.Auth;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class OAuthServiceTest {
+
+    @Test
+    void processOAuthCallback() {
+    }
+
+    @Test
+    void getCurrentUser() {
+    }
+
+    @Test
+    void testGetCurrentUser() {
+    }
+}
---
+++ src/test/java/com/hivestudent/bookme/dao/ReservationRepositoryTest.java
@@ -0,0 +1,16 @@
+package com.hivestudent.bookme.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReservationRepositoryTest {
+
+    @Test
+    void existsOverlapping() {
+    }
+
+    @Test
+    void findAllBetweenDates() {
+    }
+}
