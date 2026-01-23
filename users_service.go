f049974a6a863d74ab34b4506bff59475a4be86a
Abdulbaki <Salausmart132@gmail.com>
2026-01-23 15:54:43 +0200

    refactor getUser42 function to use oauth2 client and improve error handling

--- users_service.go
+++ users_service.go
@@ -1,42 +1,40 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"time"
+
+	"golang.org/x/oauth2"
 )
 
 // TODO look into timeout and context
-func getUser42(accessToken string, url string) (*User42, error) {
+func getUser42(ctx context.Context, oauthConfig *oauth2.Config, token *oauth2.Token) (*User42, error) {
 
-	client := &http.Client{
-		Timeout: time.Second * 15,
-	}
+	// A specialized HTTP client that handles the Authorization header
+	// and token refreshing automatically.
+	client := oauthConfig.Client(ctx, token)
 
-	req, err := http.NewRequest("GET", url, nil)
+	res, err := client.Get("https://api.intra.42.fr/v2/me") //externalize later
 	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Set("Authorization", "Bearer "+accessToken)
-	req.Header.Add("Content-Type", "application/json")
-
-	res, err := client.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("Request to get 42 user unsuccesful with status: %s", res.Status)
+		return nil, fmt.Errorf("failed to fetch user from intra: %s", err)
 	}
 	// close response body
 	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("intra api returned error status: %s", res.Status)
+	}
 
 	var user42 User42
 	decoder := json.NewDecoder(res.Body)
 	err = decoder.Decode(&user42)
 	if err != nil {
-		return nil, fmt.Errorf("failed to decode response: %w", err)
+		return nil, fmt.Errorf("failed to decode intra user data: %w", err)
 	}
 
-	log.Println(user42)
+	log.Println(user42.Name, user42.Email) //remove later
 	return &user42, nil
 }
