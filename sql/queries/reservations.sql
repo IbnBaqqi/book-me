3c16f886bba78ec7d716e1f74775fc0769180410
Abdulbaki <Salausmart132@gmail.com>
2026-01-25 21:09:56 +0200

    feat: add query and handler for fetching reservations within date range

--- sql/queries/reservations.sql
+++ sql/queries/reservations.sql
@@ -22,3 +22,19 @@ SELECT EXISTS (
       AND start_time < $3
       AND end_time > $2
 );
+
+-- name: GetAllBetweenDates :many
+SELECT 
+    r.id,
+    r.room_id,
+    r.start_time,
+    r.end_time,
+    r.user_id as created_by_id,
+    u.name as created_by_name,
+    room.name as room_name
+FROM reservations r
+INNER JOIN users u ON r.user_id = u.id
+INNER JOIN rooms room ON r.room_id = room.id
+WHERE r.start_time >= $1
+  AND r.end_time <= $2
+ORDER BY r.room_id, r.start_time;
