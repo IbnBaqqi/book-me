93b9f234b119e92d2fa2079fc4bd9efa83ccc522
Abdulbaki <Salausmart132@gmail.com>
2026-02-10 10:54:39 +0200

    refactor: inject ReservationService via constructor
    - Remove database queries dependency from Handler
    - Remove slog logger dependency
    - Accept ReservationService as constructor parameter
    - Remove inline service instantiation
    - Update import path for google package

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,20 +1,17 @@
 package handler
 
 import (
-	"github.com/IbnBaqqi/book-me/external/google"
+	"github.com/IbnBaqqi/book-me/internal/google"
 	"github.com/IbnBaqqi/book-me/internal/auth"
-	"github.com/IbnBaqqi/book-me/internal/database"
 	"github.com/IbnBaqqi/book-me/internal/email"
 	"github.com/IbnBaqqi/book-me/internal/service"
 
 	"github.com/gorilla/sessions"
 	"golang.org/x/oauth2"
-	"log/slog"
 )
 
 // Handler holds all dependencies for HTTP handlers
 type Handler struct {
-	db               *database.Queries
 	session          *sessions.CookieStore
 	oauthConfig      *oauth2.Config
 	auth             *auth.Service
@@ -23,28 +20,26 @@ type Handler struct {
 	
 	reservation		 *service.ReservationService
 	userService      *service.UserService
-	logger           *slog.Logger
 	// authService        *services.AuthService
 }
 
 // New creates a new Handler with all dependencies injected
 func New(
-	db *database.Queries,
-	sessionStore *sessions.CookieStore,
-	oauthConfig *oauth2.Config,
-	authService *auth.Service,
-	emailService *email.Service,
-	calendarService *google.CalendarService,
-	userService     *service.UserService,
+	sessionStore         *sessions.CookieStore,
+	oauthConfig          *oauth2.Config,
+	authService          *auth.Service,
+	emailService         *email.Service,
+	calendarService      *google.CalendarService,
+	reservationService   *service.ReservationService,
+	userService          *service.UserService,
 ) *Handler {
 	return &Handler{
-		db:               db,
 		session:          sessionStore,
 		oauthConfig:      oauthConfig,
 		auth:             authService,
 		email:            emailService,
 		calendar:         calendarService,
-		reservation: 	  service.NewReservationService(db, emailService, calendarService),
+		reservation: 	  reservationService,
 		userService:      userService,
 	}
 }
