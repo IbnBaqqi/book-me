ed533fa80e9c403cbef94f44f1fa6141ffa7bbb9
Abdulbaki <Salausmart132@gmail.com>
2026-02-13 00:01:15 +0200

    refactor: update Handler dependencies
    - Replace session store and OAuth config with oauth.Service
    - Remove UserService dependency
    - Simplify constructor parameters

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,45 +1,35 @@
 package handler
 
 import (
-	"github.com/IbnBaqqi/book-me/internal/google"
 	"github.com/IbnBaqqi/book-me/internal/auth"
 	"github.com/IbnBaqqi/book-me/internal/email"
+	"github.com/IbnBaqqi/book-me/internal/google"
+	"github.com/IbnBaqqi/book-me/internal/oauth"
 	"github.com/IbnBaqqi/book-me/internal/service"
-
-	"github.com/gorilla/sessions"
-	"golang.org/x/oauth2"
 )
 
 // Handler holds all dependencies for HTTP handlers
 type Handler struct {
-	session          *sessions.CookieStore
-	oauthConfig      *oauth2.Config
+	oauth            *oauth.Service
 	auth             *auth.Service
 	email            *email.Service
 	calendar         *google.CalendarService
-	
 	reservation		 *service.ReservationService
-	userService      *service.UserService
-	// authService        *services.AuthService
 }
 
 // New creates a new Handler with all dependencies injected
 func New(
-	sessionStore         *sessions.CookieStore,
-	oauthConfig          *oauth2.Config,
+	oauthService         *oauth.Service,
 	authService          *auth.Service,
 	emailService         *email.Service,
 	calendarService      *google.CalendarService,
 	reservationService   *service.ReservationService,
-	userService          *service.UserService,
 ) *Handler {
 	return &Handler{
-		session:          sessionStore,
-		oauthConfig:      oauthConfig,
+		oauth:            oauthService,
 		auth:             authService,
 		email:            emailService,
 		calendar:         calendarService,
 		reservation: 	  reservationService,
-		userService:      userService,
 	}
 }
